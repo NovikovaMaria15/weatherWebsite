678ca67ed4cb98f3068b99df160126f91b3c7ad0
NovikovaMaria15 <privalova_maria15@mail.ru>
2022-06-07 17:31:39 +0300

    deploy

--- src/pages/components/HourlyDay/HourlyDay.test.tsx
+++
@@ -1,9 +0,0 @@
-import { renderWithRedux } from 'src/specHelper';
-import HourlyDay from './HourlyDay';
-
-describe('Redux', () => {
-  it('checks 0', () => {
-    const { getByText } = renderWithRedux(<HourlyDay />);
-    expect(getByText('test')).toBeVisible();
-  });
-});
