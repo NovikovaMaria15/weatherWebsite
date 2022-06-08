99b9cee0c9c2f09a9a39fc44dc5ebce470aa75e4
NovikovaMaria15 <privalova_maria15@mail.ru>
2022-06-08 12:57:53 +0300

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes, HashRouter as Router } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import './App.css';
 
